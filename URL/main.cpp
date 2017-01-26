2f8959e0113b0b90df68e5f166696378da1159c4
Jozef <jozuel666@gmail.com>
2017-01-26 20:01:43 +0100

    dodano funkcje obliczajaca kolejne iteracje i normy z nimi zwiazane

--- URL/main.cpp
+++ URL/main.cpp
@@ -16,7 +16,7 @@ unsigned long n = 0; // Rozmiar macierzy A i wektorów B i X
 unsigned long max_liczba_iteracji = 0; // Maksymalna iloœæ iteracji
 double epsilon = 0; // Dok³adnoœæ 'epsilon'
 
-double *normy[3];
+double normy[3];
 
 void a()
 {
@@ -51,7 +51,7 @@ void a()
 	plikWejœciowy.close();
 }
 
-void b()
+int b()
 {
 	// Przekszta³canie A i B
 	for (int i = 0; i < n; i++)
@@ -73,41 +73,79 @@ void b()
 	{
 		for (int j = 0; j < n; j++)
 		{
-			alfa[i][j] = (double)(i == j ? 0 : (-(A[i][j] / A[i][i])));
+			if (i == j)
+			{
+				alfa[i][j] = 0;
+			}
+			else if (A[i][i] != 0)
+			{
+				alfa[i][j] = -(A[i][j] / A[i][i]);
+			}
+			else
+			{
+				return 1;
+			}
+			//alfa[i][j] = (double)(i == j ? 0 : (-(A[i][j] / A[i][i])));
 		}
 		beta[i] = (double)(B[i] / A[i][i]);
 	}
+	return 0;
 }
 
 void c()
 {
-	double norma[3];
+	//double norma[3];	//normy maja byc liczone z alfy
 	unsigned long it = 0;
 	double suma = 0;
-	memcpy(X, beta, (sizeof(double)*n));
-	for (int j = 0; j < n; j++)
-	{
-		norma[0] += abs(A[n - 1][j]);	//norma 1
-		norma[1] += abs(A[j][n - 1]);	//norma 2
-	}
-	
+	oX = new double[n];	//poprzednia iteracja
+	X = new double[n];
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < n; j++)
-		{
-			norma[2] += pow(A[i][j], 2);
-		}
+		X[i] = beta[i];
 	}
-	norma[2] = sqrt(norma[2]);		//norma 3
+	//memcpy(X, beta, (sizeof(double)*n));
+	//X = new double[n];
+	
 	do {
-		memcpy(oX, X, (sizeof(double)*n));
-		it++;
-		X = NULL;
+		//memcpy(oX, X, (sizeof(double)*n));	//kopiuje aktualny wektor
+		for (int i = 0; i < n; i++)
+		{
+			oX[i] = X[i];
+		}
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (j != i)
+				{
+					suma =+ alfa[i][j] * oX[j] + beta[i];	//obliczanie nowego X
+				}
+			}
+			X[i] = suma;		//zapisuje sume do aktualnego wektora
+			suma = 0;
+		}
 
-		norma[0] = 0;
-		norma[1] = 0;
-		norma[2] = 0;
-	} while ((norma[0] <= epsilon) || (norma[1] <= epsilon) || (norma[2] <= epsilon) || (it >= max_liczba_iteracji));
+		it++;
+
+		normy[0] = abs(X[n-1]);		//norma 1
+		suma = 0;
+		for (int i = 0; i < n; i++)
+		{
+			suma += abs(X[i]);
+		}
+		normy[1] = suma;		//norma 2
+		suma = 0;
+		for (int i = 0; i < n; i++)
+		{
+			suma += pow(X[i], 2);	
+		}
+		normy[2] = sqrt(suma);		//norma 3
+	} while ((normy[0] <= epsilon) || (normy[1] <= epsilon) || (normy[2] <= epsilon) || (it >= max_liczba_iteracji));
+	cout << "iteracja "<<it << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout<<X[i]<<endl;
+	}
 }
 
 void d()
@@ -118,6 +156,14 @@ void d()
 int main()
 {
 	a();
+	if (b() != 1)
+	{
+		c();
+	}
+	else
+	{
+		cout << "dzielenie przez 0";
+	}
 	for (int i = 0; i < n; i++)	//zwolnienie pamieci
 	{
 		delete[] A[i];
