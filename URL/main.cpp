415740ea609146ded4f7475fc14ab789c4f3254d
Jozef <jozuel666@gmail.com>
2017-01-26 21:36:06 +0100

    dodano funkcje D tworzącą raport z danych programu.

--- URL/main.cpp
+++ URL/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-
+#include <iomanip>
 using namespace std;
 
 double **A; // Macierz A
@@ -15,7 +15,7 @@ double *beta; // Wektor beta
 unsigned long n = 0; // Rozmiar macierzy A i wektorów B i X
 unsigned long max_liczba_iteracji = 0; // Maksymalna iloœæ iteracji
 double epsilon = 0; // Dok³adnoœæ 'epsilon'
-
+int iteracja;
 double normy[3];
 
 void a()
@@ -85,7 +85,6 @@ int b()
 			{
 				return 1;
 			}
-			//alfa[i][j] = (double)(i == j ? 0 : (-(A[i][j] / A[i][i])));
 		}
 		beta[i] = (double)(B[i] / A[i][i]);
 	}
@@ -94,7 +93,6 @@ int b()
 
 void c()
 {
-	//double norma[3];	//normy maja byc liczone z alfy
 	unsigned long it = 0;
 	double suma = 0;
 	oX = new double[n];	//poprzednia iteracja
@@ -103,14 +101,11 @@ void c()
 	{
 		X[i] = beta[i];
 	}
-	//memcpy(X, beta, (sizeof(double)*n));
-	//X = new double[n];
 	
 	do {
-		//memcpy(oX, X, (sizeof(double)*n));	//kopiuje aktualny wektor
 		for (int i = 0; i < n; i++)
 		{
-			oX[i] = X[i];
+			oX[i] = X[i];		//kopiowanie poprzedniego wektora
 		}
 		for (int i = 0; i < n; i++)
 		{
@@ -141,24 +136,73 @@ void c()
 		}
 		normy[2] = sqrt(suma);		//norma 3
 	} while ((normy[0] <= epsilon) || (normy[1] <= epsilon) || (normy[2] <= epsilon) || (it >= max_liczba_iteracji));
-	cout << "iteracja "<<it << endl;
-	for (int i = 0; i < n; i++)
-	{
-		cout<<X[i]<<endl;
-	}
+	//cout << "iteracja "<<it << endl;
+	iteracja = it;
+	//for (int i = 0; i < n; i++)
+	//{
+	//	cout<<X[i]<<endl;
+	//}
 }
 
 void d()
 {
+	ofstream plik;
+	plik.open("raport.txt");
+	plik << "Macierz A" <<endl;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			plik << setw(8) << A[i][j] << " ";
+		}
+		plik << endl;
+	}
+	plik << "Wektor B" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		plik << setw(8) << B[i] << " ";
+	}
+	plik << endl;
+	plik << "Epsilon: " << epsilon << " maxymalna ilosc iteracji " << max_liczba_iteracji << endl;
+	plik << "Alfa" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			plik << setw(8) << alfa[i][j] << " ";
+		}
+		plik << endl;
+	}
+	plik << endl;
+	plik << "Beta" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		plik << setw(8) << beta[i] << " ";
+	}
+	plik << endl;
+	plik << "Przedostatnia iteracja" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		plik << setw(8) << oX[i] << " ";
+	}
+	plik << endl;
+	plik << "Ostatnia iteracja" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		plik << setw(8) << X[i] << " ";
+	}
+	plik << endl;
+	plik << "ilosc iteracji " << iteracja;
 
 }
 
 int main()
 {
 	a();
-	if (b() != 1)
+	if (b() == 0)
 	{
 		c();
+		d();
 	}
 	else
 	{
@@ -167,7 +211,12 @@ int main()
 	for (int i = 0; i < n; i++)	//zwolnienie pamieci
 	{
 		delete[] A[i];
+		delete[] alfa[i];
 	}
+	delete[] X;
+	delete[] oX;
+	delete[] beta;
+	delete[] alfa;
 	delete[] A;
 	delete[] B;
 	cin.ignore();
